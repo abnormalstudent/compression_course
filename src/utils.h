79909785ea5667f80f6574a8a19606337daa82e9
Martynov Eduard <57013219+abnormalstudent@users.noreply.github.com>
2020-09-30 23:07:20 +0300

    Add files via upload

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,10 @@
+//
+// Created by eduard on 30/09/2020.
+//
+
+#ifndef CPP_COMPRESSOR_UTILS_H
+#define CPP_COMPRESSOR_UTILS_H
+
 #pragma once
 
 enum {
@@ -6,19 +13,21 @@ enum {
     BWT
 };
 
-typedef struct CompressOptions {
+struct CompressOptions {
     char *ifile;
     char *ofile;
     char mode;
     int  method;
-} CompressOptions;
+};
 
 CompressOptions *parse_args(int argc, char **argv);
 
-CompressOptions *default_config(void);
+CompressOptions *default_config();
 
 void print_config(CompressOptions *opts);
 
 void free_compress_opts(CompressOptions *opts);
 
 int can_open_file(const char *filename);
+
+#endif //CPP_COMPRESSOR_UTILS_H
