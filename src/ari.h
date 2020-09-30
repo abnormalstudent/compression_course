e867f93fda47e48089e35c5a077f2800d2ba17ca
Martynov Eduard <57013219+abnormalstudent@users.noreply.github.com>
2020-09-30 23:07:57 +0300

    C++ main instead of C

--- src/ari.h
+++ src/ari.h
@@ -1,4 +1,13 @@
+//
+// Created by eduard on 30/09/2020.
+//
+
+#ifndef CPP_COMPRESSOR_ARI_H
+#define CPP_COMPRESSOR_ARI_H
+
 #pragma once
 
 void compress_ari(char *ifile, char *ofile);
 void decompress_ari(char *ifile, char *ofile);
+
+#endif //CPP_COMPRESSOR_ARI_H
